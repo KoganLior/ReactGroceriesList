9d4d829612fcce7d52803fecef8d12443c9d0681
KoganLior <liorrrr100@gmail.com>
2022-08-14 11:30:34 +0300

    made a first page design to see how the lists of items would look like

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
 import Grocery from "./Grocery";
-import "./grocery.css"
+import "./grocery.css";
 function App() {
   return (
     <div className="App">
-      <Grocery
-      name="grocery 1"
-      price="1,000,000$"
-      quantity="1"
-      ></Grocery>
+      <div className="groceriesList">
+        <h2 className="listTitle">Groceries List</h2>
+        <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+        <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+        <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+        <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+        <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+        <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+        <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+      </div>
+
+      <div className="shoppingList">
+      <h2 className="listTitle">Shopping List List</h2>
+      <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+      <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+      <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+      <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+      <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+      <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+      <Grocery name="grocery 1" price="1,000,000$" quantity="1"></Grocery>
+      </div>
     </div>
   );
 }
--- my-app/src/Grocery.js
+++ my-app/src/Grocery.js
@@ -2,7 +2,7 @@ function Grocery(props){
 
     return(
         <div className="grocery">
-        <img src="https://via.placeholder.com/150" alt="no image" className="groceryThumbnail"></img>
+        <img src="https://via.placeholder.com/150" alt="product" className="groceryThumbnail"></img>
         <div className="groceryInfo">
           <p>Name : {props.name}</p>
           <p>Price :{props.price}</p>
